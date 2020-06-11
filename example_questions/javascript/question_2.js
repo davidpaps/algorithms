d55b3829c63dd1c01aa176f464ada1a8a5676513
David Papamichael <davidpapamichael@gmail.com>
2020-06-11 15:51:12 +0100

    2nd option of question two added

--- example_questions/javascript/question_2.js
+++ example_questions/javascript/question_2.js
@@ -1,12 +1,10 @@
 // Q2: Given 2 strings, return an array with characters that appear equal times in each string.
-
-// EG ‘abc’, ‘bcd’ => [‘b’, ‘c’] both b and c appear once in each string
-// EG ‘aaaaaaannd’, ‘ann’ => [‘n’] n appears twice in each string. a is 7 in first string and only 1 in second string so they aren’t equal.
 // EG ‘hello’, ‘world’ => [‘o’] o appears once in each string. L appears in both, twice in first and only once in second so not equal.
 
 let stringOne = "hello";
 let stringTwo = "world";
 
+// 1
 const equalString = (stringOne, stringTwo) => {
   let arrayOne = stringOne.split("");
   let arrayTwo = stringTwo.split("");
@@ -37,3 +35,27 @@ const equalString = (stringOne, stringTwo) => {
 };
 
 console.log(equalString(stringOne, stringTwo));
+
+// 2
+const equalStringTwo = (stringOne, stringTwo) => {
+  let arrayOne = stringOne.split("");
+  let arrayTwo = stringTwo.split("");
+  let countOne = {};
+  let countTwo = {};
+  let finalArray = [];
+  for (let i = 0; i < stringOne.length && i < stringTwo.length; i++) {
+    let count = stringOne.split(arrayOne[i]).length - 1;
+    countOne[arrayOne[i]] = count;
+
+    let countT = stringTwo.split(arrayTwo[i]).length - 1;
+    countTwo[arrayTwo[i]] = countT;
+  }
+  Object.keys(countOne).forEach((key) => {
+    if (countOne[key] === countTwo[key]) {
+      finalArray.push(key);
+    }
+  });
+  return finalArray;
+};
+
+console.log(equalStringTwo(stringOne, stringTwo));
